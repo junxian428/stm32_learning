5cd58f4485c24be57781def7c12ff1ba00121f3f
junxian428 <holeyiho@gmail.com>
2023-02-05 23:27:24 +0800

    STM32 Blue Pill

---
+++ 2.1/Objects/system_stm32f10x.d
@@ -0,0 +1,8 @@
+./objects/system_stm32f10x.o: system_stm32f10x.c \
+  C:\Users\junxian428\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\stm32f10x.h \
+  C:\Users\junxian428\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\junxian428\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\core_cmInstr.h \
+  C:\Users\junxian428\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\cmsis_armcc_V6.h \
+  C:\Users\junxian428\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\core_cmFunc.h \
+  C:\Users\junxian428\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\system_stm32f10x.h
